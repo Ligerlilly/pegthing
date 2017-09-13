e5907b7fc8ab93d67445c726c8216f00bb9ef9be
Jason Lane <quizathaderat@yahoo.com>
2017-09-12 22:26:04 -0700

    Initial commit

---
+++ src/pegthing/core.clj
@@ -0,0 +1,32 @@
+(ns pegthing.core
+  (require [clojure.set :as set])
+  (:gen-class))
+
+(declare secussful-move prompt-move game-over query-rows)
+
+(defn tri*
+  "Generates lazy sequence of triangular numbers"
+  ([] (tri* 0 1))
+  ([sum n]
+    (let [new-sum (+ sum n)]
+      (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
+
+(def tri (tri*))
+
+(defn triangular?
+  "Is the number triangular? e.g. 1, 3, 6, 10, 15, etc"
+  [n]
+  (= n (last (take-while #(>= n %) tri))))
+
+
+(defn triangular?
+  "Is the number triangular? e.g. 1, 3, 6, 10, 15, etc"
+  [n]
+  (= n (last (take-while #(>= n %) tri))))
+
+
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
